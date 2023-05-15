f0d62087573b388e0c3f57d89b762b055c3dfb34
Jason Zheng <jaszheng2001@gmail.com>
2023-05-15 16:45:46 -0400

    Update thumbnail to not required

--- models/mapcard-model.js
+++ models/mapcard-model.js
@@ -6,7 +6,7 @@ const MapCardSchema = new Schema(
   {
     mapId: { type: String, required: true },
     mapName: { type: String, required: true },
-    thumbnail: { type: String, required: true },
+    thumbnail: { type: String },
     ownedUser: { type: String, required: true },
     published: { type: Boolean, required: true },
     likedUsers: { type: [String], required: true },
